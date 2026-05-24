4354bd6a47fa52578cb5f04699949477ac1f0cac
Faila028 <norfailasufah28@students.unnes.ac.id>
2026-05-24 13:47:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ if uploaded_file is not None:
         model = ExponentialSmoothing(
             data_produk,
             trend='add',
-            seasonal='add',
+            seasonal='multiplicative',
             seasonal_periods=12
         ).fit()
 
